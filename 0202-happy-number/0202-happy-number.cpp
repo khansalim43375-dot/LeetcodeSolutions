82b77f7ceea342b0b3aaad8f87f45099c49b7f25
Mohd Salim <khansalim43375@gmail.com>
2026-08-03 15:41:39 +0530

    Time: 0 ms (100%), Space: 7.8 MB (68.48%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int next(int n) {
+        int sum = 0;
+        while (n) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = next(n);
+
+        while (fast != 1 && slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+
+        return fast == 1;
+    }
+};
