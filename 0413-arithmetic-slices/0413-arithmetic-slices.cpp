a60f2a5f03fbab732d3a64f12ed3afe71ce04158
Mohd Salim <khansalim43375@gmail.com>
2026-09-02 18:05:18 +0530

    Time: 0 ms (100%), Space: 10.8 MB (84.64%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int count = 0;
+        int curr = 0;
+
+        for(int i = 2; i < nums.size(); i++) {
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                curr++;
+                count += curr;
+            }
+            else {
+                curr = 0;
+            }
+        }
+
+        return count;
+    }
+};
